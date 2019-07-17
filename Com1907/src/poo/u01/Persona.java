a1807c12540f3e56efeb676d1db939710e7c77f2
Usuario <Usuario@192.168.2.242>
2019-07-17 12:18:48 -0300

    @Ramiro Mi20190717

--- Com1907/src/poo/u01/Persona.java
+++ Com1907/src/poo/u01/Persona.java
@@ -6,14 +6,15 @@ public class Persona {
 	private int edad;
 	private String genero;
 
-	public int getEdad() {
-		return edad;
-	}
-	
 	public Persona(String nombre, double altura, int edad, String genero) {
 		this.nombre = nombre;
 		this.altura = altura;
 		this.edad = edad;
 		this.genero = genero;
 	}
+
+	public int getEdad() {
+		return edad; 
+	}
+
 }
