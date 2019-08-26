0b2a430a8cc40729dcd1e64057f06c06b01f29ee
Usuario <Usuario@192.168.2.242>
2019-08-26 12:17:46 -0300

    @ramirocodoacodo Lu20190826

--- Com1907/src/poo/ejemplos/MainAuto.java
+++ Com1907/src/poo/ejemplos/MainAuto.java
@@ -5,10 +5,16 @@ public class MainAuto {
 	public static void main(String[] args) {
 		Auto miAuto = new Auto(); // Creo el objeto auto (instancio la clase Auto)
 		Auto auto2 = new Auto(); // Creo otro objeto auto (instancio la clase Auto)
+		Vehiculo veh1 = new Vehiculo();
 		
-		miAuto.encender();
+		miAuto.arrancar();
 		miAuto.acelerar();
+		veh1.frenar();
+		System.out.println(veh1.toString());
+		System.out.println(miAuto.toString());
 		
+		Motor m1 = new Motor();
+		veh1.setMotor(m1);
 	}
 
 }
